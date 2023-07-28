7c36086c591afdb1c08db576cfb26991abb835a7
samiksha <sammyjadhav05@gmail.com>
2023-07-28 18:31:29 +0100

    test

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -19,7 +19,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count          = 2
     vm_size             = "Standard_D2_v2"
     vnet_subnet_id      = "/subscriptions/e7309d26-3161-4734-8495-0967e305fe8e/resourceGroups/api-demo/providers/Microsoft.Network/virtualNetworks/Linux-vm-vnet/subnets/aks-subnet"  
-    availability_zones  = [1]  # Optional: for multi-zone setup
     enable_auto_scaling = false  # Optional: set to true for auto-scaling
   }
 
