e927f09899ffb33affd9f63df2757bf068f2965f
samiksha <sammyjadhav05@gmail.com>
2023-07-28 20:04:38 +0100

    test

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name                = "default"
     node_count          = 2
     vm_size             = "Standard_D2_v2"
-    vnet_subnet_id      = "/subscriptions/e7309d26-3161-4734-8495-0967e305fe8e/resourceGroups/api-demo/providers/Microsoft.Network/virtualNetworks/Linux-vm-vnet/subnets/aks-subnet"  
+    vnet_subnet_id      = "/subscriptions/e7309d26-3161-4734-8495-0967e305fe8e/resourcegroups/api-demo/providers/Microsoft.Network/virtualNetworks/aks-vnet/subnets/aks-subnet"
     enable_auto_scaling = false  # Optional: set to true for auto-scaling
   }
 
